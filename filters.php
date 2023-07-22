a5c8ba23a46f581489bb55dfc32b6c4a2bbadb9b
Mason Lawlor <mason.lawlor@me.com>
2023-07-22 04:12:51 -0600

    Product parameter in Gf shortcode - v0.1-alpha-0.18

--- filters.php
+++ filters.php
@@ -8,3 +8,17 @@ add_filter( 'gform_submit_button_25', function ( $button, $form ) {
 add_filter( 'gform_submit_button_26', function ( $button, $form ) {
   return "<div class='col-sm'><button class='btn button gform_button' id='gform_submit_button_{$form['id']}'><span class='btn-copy mr-3'>Subscribe to Sensei</span><i class='fas fa-arrow-right'></i></button></div>";
 }, 10, 2);
+
+// Gravity Forms passes custom parameter through shortcode
+function customize_gravity_form_shortcode($out, $pairs, $atts) {
+    // Check if the 'product' attribute is set
+    if (isset($atts['product'])) {
+        // Add a filter to pre-populate the field
+        add_filter('gform_field_value_product', function() use ($atts) {
+            return $atts['product'];
+        });
+    }
+
+    return $out;
+}
+add_filter('shortcode_atts_gravityforms', 'customize_gravity_form_shortcode', 10, 3);
