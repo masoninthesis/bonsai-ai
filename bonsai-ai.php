5de2c10f120ef6799e34b3eeadbcdabc0472087e
Mason Lawlor <mason.lawlor@me.com>
2023-07-25 02:18:02 -0600

    Updated version number - v0.1-alpha-0.24

--- bonsai-ai.php
+++ bonsai-ai.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Bonsai AI
  * Description: A WordPress plugin that adds AI functionalities such as Sensei AI, and SenseiOS
- * Version: 0.0.1-alpha-0.22
+ * Version: 0.0.1-alpha-0.24
  * Author: Jackalope Labs
  * Author URI: https:/bonsai.so/
  */
