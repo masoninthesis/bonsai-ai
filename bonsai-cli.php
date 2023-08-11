92bcb9451de2dd8e744e67df9595695ad90ea351
Mason Lawlor <mason.lawlor@me.com>
2023-08-11 01:23:45 -0600

    Changed followup.php to autoresponse.php - 0.1-alpha-0.43

--- bonsai-cli.php
+++ bonsai-cli.php
@@ -8,7 +8,7 @@ class Bonsai_Command extends WP_CLI_Command {
      */
     public function deshi($args, $assoc_args) {
         if(isset($args[0]) && $args[0] === 'autoresponse') {
-            include(plugin_dir_path(__FILE__) . 'followup.php');
+            include(plugin_dir_path(__FILE__) . 'autoresponse.php');
         } else {
             WP_CLI::error("Invalid command for deshi.");
         }
