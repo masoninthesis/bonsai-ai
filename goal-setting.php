82f595d0fa89f0f6683bc1566499bd54f36303e0
Mason Lawlor <mason.lawlor@me.com>
2023-09-15 04:19:24 -0600

    Added SenseiOS goal setting - alpha-0.70

--- goal-setting.php
+++ goal-setting.php
@@ -1,37 +1,50 @@
 <?php
+// Include Parsedown class if not already included
+require_once 'vendor/autoload.php';
+
 // This file handles functionality related to chat system
 
-// Post Creation
+// Register the function for multiple forms
 add_action('gform_after_submission_32', 'create_smart_goal', 10, 2);
+add_action('gform_after_submission_40', 'create_smart_goal', 10, 2);
 
 function create_smart_goal($entry, $form) {
-    // extract data from the form
+    // Check form ID to ensure it's one of the forms we're interested in
+    if ($form['id'] != 32 && $form['id'] != 40) {
+        return;
+    }
+
+    // Initialize Parsedown
+    $parsedown = new Parsedown();
+
+    // Extract data from the form
     $post_title = rgar($entry, '1');
     $goal = rgar($entry, '1');
-    $response = GFCommon::replace_variables('{openai_feed_60}', $form, $entry);
+    $response_raw = GFCommon::replace_variables('{openai_feed_60}', $form, $entry);
+    $response = $parsedown->text($response_raw);  // Markdown parsing
 
-    // construct the post content using the question and response
-    $post_content = '<div class="alert alert-info">'.$goal.'</div><div class="alert alert-success my-3 ml-5">'.$response.'</div>';
+    // Construct the post content using the question and response
+    $post_content = '<div class="alert alert-info">' . $goal . '</div><div class="alert alert-success my-3 ml-5">' . $response . '</div>';
 
-    // get the category object by slug
+    // Get the category object by slug
     $category = get_term_by('slug', 'goals-journal-entries', 'category');
 
-    // prepare the post data
+    // Prepare the post data
     $post_data = array(
-        'post_title'   => wp_strip_all_tags($post_title),
-        'post_content' => $post_content,
-        'post_status'  => 'publish',
-        'post_type'    => 'post',
-        'post_author'  => get_current_user_id(),
-        'comment_status' => 'open',
-        'post_category' => array( $category->term_id ),
+        'post_title'    => wp_strip_all_tags($post_title),
+        'post_content'  => $post_content,
+        'post_status'   => 'publish',
+        'post_type'     => 'post',
+        'post_author'   => get_current_user_id(),
+        'comment_status'=> 'open',
+        'post_category' => array($category->term_id),
     );
 
-    // insert the post
+    // Insert the post
     $post_id = wp_insert_post($post_data);
 
-    // redirect to the newly created post
-    if($post_id){
+    // Redirect to the newly created post
+    if ($post_id) {
         wp_redirect(get_permalink($post_id));
         exit;
     }
