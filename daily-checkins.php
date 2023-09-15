b2c5db5ea0084d5b9359ad3a5b66172f5ba44e0f
Mason Lawlor <mason.lawlor@me.com>
2023-09-15 17:39:57 -0600

    SenseiAI daily checkin Kickstart question working, next is the AWE question as a comment - alpha-0.72

--- daily-checkins.php
+++ daily-checkins.php
@@ -1,25 +1,62 @@
 <?php
-// Daily Check-in Form creates a post
-add_action('gform_after_submission_29', function($entry, $form) {
-    // Get the value of the field with the ID of 6
-    $field_6_value = rgar($entry, '6');
+// Include Parsedown class
+require_once 'vendor/autoload.php';
 
-    // Prepare the title and content for the post
-    $title = rgar($entry, '3');  // replace 3 with the field ID of the title
-    $content = rgar($entry, '5');  // This assumes that the field ID of the checklist is 5
-    $content .= "\n\n" . $field_6_value;  // Append the value of field 6 to the content
+// This file handles functionality related to daily checkin
+// Ask Sensei Form
+// Post Creation
+add_action('gform_after_submission_29', 'create_daily_checkin', 10, 2);
+
+function create_daily_checkin($entry, $form) {
+    // Initialize Parsedown
+    $parsedown = new Parsedown();
+
+    // extract data from the form
+    $post_title = rgar($entry, '3');
+    $checkin = rgar($entry, '6');
+    $response_raw = GFCommon::replace_variables('{openai_feed_89}', $form, $entry);
+    $response = $parsedown->text($response_raw);
+    $post_content = '<div class="alert alert-info">'.$checkin.'</div><div class="alert alert-success my-3 ml-5">'.$response.'</div>';
+
+    // Identify the Sensei user ID
+    $current_post_id = get_the_ID();
+    $sensei_user_id = get_post_field('post_author', $current_post_id);
 
     // Get categories by slug
     $cat_journal_entries = get_category_by_slug('journal-entries');
     $cat_checkin_journal_entries = get_category_by_slug('checkin-journal-entries');
 
-    // Create the post
-    $post_id = wp_insert_post(array(
-        'post_title'    => $title,
-        'post_content'  => $content,
+    // prepare the post data
+    $post_data = array(
+        'post_title'    => wp_strip_all_tags($post_title),
+        'post_content'  => $post_content,
         'post_status'   => 'publish',
+        'post_type'     => 'post',
         'post_author'   => get_current_user_id(),
+        'comment_status'=> 'open',
         'post_category' => array( $cat_journal_entries->term_id, $cat_checkin_journal_entries->term_id ),
-        'comment_status' => 'open',  // enable comments
-    ));
-}, 10, 2);
+    );
+
+    // insert the post
+    $post_id = wp_insert_post($post_data);
+
+    // Fetch the senseios_fields using the merge tag
+    $senseios_fields = GFCommon::replace_variables('{senseios_fields}', $form, $entry);
+
+    // Check the data type
+    if (is_array($senseios_fields)) {
+        $senseios_fields = json_encode($senseios_fields);
+    }
+
+    // Add the senseios_fields as a meta field to the post
+    add_post_meta($post_id, 'senseios_fields', $senseios_fields);
+
+    // Add the Deshi's user ID as a meta field to the post
+    add_post_meta($post_id, 'deshi_op', get_current_user_id());
+
+    // redirect to the newly created post
+    if ($post_id) {
+        wp_redirect(get_permalink($post_id));
+        exit;
+    }
+}
